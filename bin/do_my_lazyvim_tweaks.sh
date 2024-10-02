dc722b0df1454c16ca35e104b94b70763b1365aa
Jason Brunelle <jasonmb626@gmail.com>
2024-10-02 06:27:45 -0500

    don't assume user, use ~. Change to sh shell

--- bin/do_my_lazyvim_tweaks.sh
+++ bin/do_my_lazyvim_tweaks.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 set -u
 
-sed -i 's/local auto_select = true/local autoselect = vim.g.cmp_auto_select or true/' /home/jason/.local/share/nvim/lazy/LazyVim/lua/lazyvim/plugins/coding.lua
-cmp_auto_select_check=$(grep 'vim\.g\.cmd_auto_select[[:space:]]*=' /home/jason/.config/nvim/lua/config/options.lua)
+sed -i 's/local auto_select = true/local autoselect = vim.g.cmp_auto_select or true/' ~/.local/share/nvim/lazy/LazyVim/lua/lazyvim/plugins/coding.lua
+cmp_auto_select_check=$(grep 'vim\.g\.cmd_auto_select[[:space:]]*=' ~/.config/nvim/lua/config/options.lua)
 if [[ -z $cmp_auto_select_check ]]; then
-  echo "vim.g.cmp_auto_select = true" >/home/jason/.config/nvim/lua/config/options.lua
+  echo "vim.g.cmp_auto_select = true" >~/.config/nvim/lua/config/options.lua
 fi
